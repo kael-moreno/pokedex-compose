fce7b8ea5ba498cd9994f3094cd1416779de4639
kaelmoreno-cp <kael.moreno@coreproc.com>
2025-07-01 23:25:45 +0800

    Add navigation transitions to NavHost
    This commit introduces custom enter and exit transitions for the "details/{id}" route in the NavHost.
    
    Specifically, the following changes were made:
    - Added fade-in and slide-in animations for entering the details screen.
    - Added fade-out and slide-out animations for exiting the details screen.
    - Set the default enter and exit transitions for the NavHost to `EnterTransition.None` and `ExitTransition.None` respectively.

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -4,6 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
@@ -22,11 +31,39 @@ class MainActivity : ComponentActivity() {
         setContent {
             PokedexTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "home") {
+                NavHost(
+                    navController = navController,
+                    startDestination = "home",
+                    enterTransition = { EnterTransition.None},
+                    exitTransition = { ExitTransition.None}
+                ) {
                     composable("home") {
                         Main(navController)
                     }
-                    composable("details/{id}") { backStackEntry ->
+                    composable(
+                        "details/{id}",
+                        enterTransition = {
+                            fadeIn(
+                                animationSpec = tween(
+                                    300, easing = LinearEasing
+                                )
+                            ) + slideIntoContainer(
+                                animationSpec = tween(300, easing = EaseIn),
+                                towards = AnimatedContentTransitionScope.SlideDirection.Start
+                            )
+                        },
+                        exitTransition = {
+                            fadeOut(
+                                animationSpec = tween(
+                                    300, easing = LinearEasing
+                                )
+                            ) + slideOutOfContainer(
+                                animationSpec = tween(300, easing = EaseOut),
+                                towards = AnimatedContentTransitionScope.SlideDirection.End
+                            )
+                        }
+
+                    ) { backStackEntry ->
                         val id = backStackEntry.arguments?.getString("id")
                         DetailsScreen(id, navController)
                     }
